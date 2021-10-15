ff04d03c4e111350aa1083d187907a834531fc54
cowdingus <zaki.nadhif45@gmail.com>
2021-10-15 13:05:09 +0700

    Write beranda.php

---
+++ beranda.php
@@ -0,0 +1,49 @@
+<?php
+include "header.php";
+?>
+<h2>Ayam</h2>
+<div class="row">
+	<?php
+	include "db.php";
+
+	$query = "SELECT * FROM produk";
+
+	if ($stmt = $conn->prepare($query)) {
+		$stmt->execute();
+
+		if ($stmt->error) {
+			die("Failed to execute MySQLi statement: " . $stmt->error);
+		}
+
+		$stmt->bind_result($id, $nama, $deskripsi, $harga, $foto_produk);
+
+		$index = 0;
+		while ($stmt->fetch()) {
+			$index++;
+	?>
+
+			<div class="col-md-3">
+				<div class="card">
+					<img src="<?= $foto_produk ?>" class="card-img-top">
+					<div class="card-body">
+						<h5 class="card-title"><?= $nama ?></h5>
+						<p class="card-text"><?= substr($deskripsi, 0, 20) ?></p>
+						<a href="beli_produk.php?id=<?= $id ?>" class="btn btn-primary">Beli</a>
+					</div>
+				</div>
+			</div>
+
+	<?php
+		}
+
+		$stmt->close();
+	} else {
+		die("Failed to prepare() statement: " . $conn->error);
+	}
+
+	$conn->close();
+	?>
+</div>
+<?php
+include "footer.php";
+?>
