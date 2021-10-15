062f56c4a7fe799e4db9d7b576325eb5cad2d98c
cowdingus <zaki.nadhif45@gmail.com>
2021-10-15 13:02:20 +0700

    Remove connected successfully message in db.php

--- db.php
+++ db.php
@@ -11,5 +11,4 @@ $conn = new mysqli($db_servername, $db_username, $db_password, $db_database);
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected to database successfully\n";
 ?>
