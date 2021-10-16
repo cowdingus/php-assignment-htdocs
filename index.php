1a7ced3dccb969f62273edc8eb08a416ceecf4e8
cowdingus <zaki.nadhif45@gmail.com>
2021-10-16 10:23:49 +0700

    Rewrite index.php using header.php + footer.php

--- index.php
+++ index.php
@@ -1,67 +1,4 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-		integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-		integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-		crossorigin="anonymous"></script>
-
-	<link rel="stylesheet" href="/public/style.css">
-
-	<title> _Turkey. - Toko Ayam </title>
-</head>
-
-<body>
-	<header>
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			<div class="container-fluid">
-				<a class="navbar-brand" href="/index.php">
-					<img src="/public/turkey-svgrepo-com.svg" alt="" width="45" height="36">
-				</a>
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-					data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-					aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-						<li class="nav-item">
-							<a class="nav-link active" aria-current="page" href="#">Home</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">Pelanggan</a>
-						</li>
-						<li class="nav-item dropdown">
-							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-								data-bs-toggle="dropdown" aria-expanded="false">
-								Keranjang
-							</a>
-							<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-								<li><a class="dropdown-item" href="#">Sudah dibeli</a></li>
-								<li><a class="dropdown-item" href="#">Ingin dibeli</a></li>
-								<li>
-									<hr class="dropdown-divider">
-								</li>
-								<li><a class="dropdown-item" href="#">Semua</a></li>
-							</ul>
-						</li>
-					</ul>
-					<form class="d-flex">
-						<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-						<button class="btn btn-outline-success" type="submit">Search</button>
-					</form>
-				</div>
-			</div>
-		</nav>
-	</header>
-
+<?php include "header.php"; ?>
 	<div class="container-fluid px-5">
 		<div class="col">
 			<h3 class="mt-4 mb-3"> Tampilan Admin </h3>
@@ -72,12 +9,4 @@
 			</ul>
 		</div>
 	</div>
-
-	<footer class="footer">
-		<div class="container">
-			<span class="text-muted">_Turkey. -- Toko Ayam</span>
-		</div>
-	</footer>
-</body>
-
-</html>
+<?php include "footer.php"?>
