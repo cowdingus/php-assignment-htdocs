2cd6ad02a13642b3e509f27b9935bf5edc95600e
cowdingus <zaki.nadhif45@gmail.com>
2021-10-16 17:32:30 +0700

    Modify index.php

--- index.php
+++ index.php
@@ -7,6 +7,12 @@
 				<li><a href="tampil_petugas.php">Petugas</a></li>
 				<li><a href="tampil_produk.php">Produk</a></li>
 			</ul>
+			<h4>Penambahan</h4>
+			<ul>
+				<li><a href="tambah_pelanggan.html">Pelanggan</a></li>
+				<li><a href="tambah_petugas.html">Petugas</a></li>
+				<li><a href="tambah_produk.html">Produk</a></li>
+			</ul>
 		</div>
 	</div>
 <?php include "footer.php"?>
