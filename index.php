eea83af4320f024779e7d0cf7891d6f7bf1093a1
cowdingus <zaki.nadhif45@gmail.com>
2021-10-18 18:38:44 +0700

    Modify index.php to display conditional interfaces

--- index.php
+++ index.php
@@ -1,18 +1,34 @@
 <?php include "header.php"; ?>
-	<div class="container-fluid px-5">
-		<div class="col">
+<div class="container-fluid px-5">
+	<div class="col">
+		<?php
+		if ($_SESSION["level"] == "pelanggan") {
+			include "utilities.php";
+			echo generate_redirect("beranda.php");
+		}
+		?>
+		<?php if ($_SESSION["level"] == "admin") : ?>
 			<h3 class="mt-4 mb-3"> Tampilan Admin </h3>
 			<ul>
 				<li><a href="tampil_pelanggan.php">Pelanggan</a></li>
 				<li><a href="tampil_petugas.php">Petugas</a></li>
 				<li><a href="tampil_produk.php">Produk</a></li>
 			</ul>
+
 			<h4>Penambahan</h4>
 			<ul>
 				<li><a href="tambah_pelanggan.html">Pelanggan</a></li>
 				<li><a href="tambah_petugas.html">Petugas</a></li>
 				<li><a href="tambah_produk.html">Produk</a></li>
 			</ul>
-		</div>
+		<?php endif ?>
+
+		<?php if ($_SESSION["level"] == "kasir") : ?>
+			<h4>Penambahan</h4>
+			<ul>
+				<li><a href="tambah_pelanggan.html">Pelanggan</a></li>
+			</ul>
+		<?php endif ?>
 	</div>
-<?php include "footer.php"?>
+</div>
+<?php include "footer.php" ?>
